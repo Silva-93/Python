4c69c7e2051642625da10a7f80487bc29a99cf5d
Silva-93 <117053314+Silva-93@users.noreply.github.com>
2022-12-05 20:19:04 -0300

    update

---
+++ PYTHON/tipos_dados.py
@@ -0,0 +1,26 @@
+"""
+String - str -> Textos | São digitados dentro de aspas simples '' ou aspas duplas ""
+
+Int -> Números inteiros | Não são digitados dentro de aspas simples ou duplas
+
+Float -> Números "flutuantes" números com vírgula |  Não são digitados dentro de aspas simples ou duplas
+
+Boolean - bool -> Valores lógicos, verdadeiro ou falso
+"""
+
+print('Isso é uma String')  # Isso é uma string
+
+print(1, 5, 10)  # Isso é um int
+
+print(1.2, 10.12)  # Isso é um float
+
+print(True)  # Verdadeiro | bool
+
+print(False)  # Falso | bool
+
+
+#  CONVERTENDO VALORE
+
+print(type(int('1')))  #  Converteu uma str em um int
+print(type(str(1)))  #  Converteu um int em uma str
+
---
+++ PYTHON/variaveis.py
@@ -0,0 +1,52 @@
+"""
+    Variáveis são valores armazenados na memória para serem usados postariormente em outras partes do código.
+    sintaxe.
+    name = value | O sinal de "=" é usada para atribuir um valor a variável, esse valor pode ser de qualquer tipo 
+
+    exemplos.:
+    nome = 'Jouber'
+    idade = 29
+    altura = 1.75
+    ... 
+"""
+
+frase = 'Uma mente que se abre a uma nova ideia jamais voltará ao seu temanho original.'
+
+numero1 = 10
+numero2 = 20
+soma = numero1 + numero2
+
+print(frase)
+print(numero1)
+print(numero2)
+print(soma)
+
+#  Exercício
+
+nome = 'Jouber'
+sobrenome = 'Vicente'
+idade = 29
+altura = 1.75
+ano_nascimento = 2022 - idade
+maior_de_idade = idade >= 18
+
+print(f'Nome: {nome}')
+print(f'Sobrenome: {sobrenome}')
+print(f'Idade: {idade}')
+print(f'Ano de nascimento: {ano_nascimento}')
+print(f'É maior de idade: {maior_de_idade}')
+print(f'Altura em metros: {altura}m')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
