240eb7453c3619d933e4a0b6abbd95e09bce3aca
Silva-93 <117053314+Silva-93@users.noreply.github.com>
2022-12-05 18:47:29 -0300

    UPDATE

---
+++ PYTHON/aula1.py
@@ -0,0 +1,8 @@
+print(10, 20, 30, 40, 50, sep=' - ')  # o "sep=''" define um separador para os parâmetro. Por padrão é o "espaço"
+print(60, 70, 80, 90, 100, sep=' -> ')  # o "sep=''" define um separador para os parâmetro. Por padrão é o "espaço
+
+print(10, 20, 30, 40, 50, sep=' - ', end=' continuou ')  # o "end=''" Faz com que não haja uma quebra de linha
+print(60, 70, 80, 90, 100, sep=' -> ')  
+
+
+
