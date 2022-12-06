d6f828b52d9288281d4bd505ed58666cce15c2d9
Silva-93 <117053314+Silva-93@users.noreply.github.com>
2022-12-05 23:07:42 -0300

    Create operadores.py

---
+++ PYTHON/operadores.py
@@ -0,0 +1,34 @@
+"""
+    + -> Adição
+
+    - -> Subtração
+
+    * -> Multiplicação
+
+    / -> Divisão 
+
+    // -> Divisão inteira
+
+    ** -> Potência
+"""
+
+adicao = 10 + 10
+print(f'Adição {adicao}')
+
+subtracao = 10 - 5
+print(f'Subtração {subtracao}')
+
+multiplicacao = 10 * 10
+print(f'Multiplicação {multiplicacao}')
+
+divisao = 10 / 2
+print(f'Divisão {divisao}')
+
+divisao_inteira = 10 // 2.2
+print(f'Divisão inteira {divisao_inteira}')
+
+potencia = 10**2
+print(f'Potência {potencia}')
+
+resto_divisao = 55 % 2
+print(f'Resto da divisão {resto_divisao}')
