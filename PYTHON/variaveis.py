55724ffd2ab0deb719f925ef92301ac34a7c2e03
Silva-93 <117053314+Silva-93@users.noreply.github.com>
2022-12-07 17:48:16 -0300

    Update variaveis.py

--- PYTHON/variaveis.py
+++ PYTHON/variaveis.py
@@ -10,41 +10,71 @@
     ... 
 """
 
-frase = 'Uma mente que se abre a uma nova ideia jamais voltará ao seu temanho original.'
+# frase = 'Uma mente que se abre a uma nova ideia jamais voltará ao seu temanho original.'
 
-numero1 = 10
-numero2 = 20
-soma = numero1 + numero2
+# numero1 = 10
+# numero2 = 20
+# soma = numero1 + numero2
 
-print(frase)
-print(numero1)
-print(numero2)
-print(soma)
+# print(frase)
+# print(numero1)
+# print(numero2)
+# print(soma)
 
-#  Exercício
+# #  Exercício
 
-nome = 'Jouber'
-sobrenome = 'Vicente'
-idade = 29
-altura = 1.75
-ano_nascimento = 2022 - idade
-maior_de_idade = idade >= 18
+# nome = 'Jouber'
+# sobrenome = 'Vicente'
+# idade = 29
+# altura = 1.75
+# ano_nascimento = 2022 - idade
+# maior_de_idade = idade >= 18
 
-print(f'Nome: {nome}')
-print(f'Sobrenome: {sobrenome}')
-print(f'Idade: {idade}')
-print(f'Ano de nascimento: {ano_nascimento}')
-print(f'É maior de idade: {maior_de_idade}')
-print(f'Altura em metros: {altura}m')
-
-
+# print(f'Nome: {nome}')
+# print(f'Sobrenome: {sobrenome}')
+# print(f'Idade: {idade}')
+# print(f'Ano de nascimento: {ano_nascimento}')
+# print(f'É maior de idade: {maior_de_idade}')
+# print(f'Altura em metros: {altura}m')
 
 
 
+'''
+    CONSTANTE = variáveis que não mudam de valor
+    Muitas condições no mesmo if (ruim)
+
+    Quando se quer criar uma variável constante, a variável é feita com todas as letras maiúsculas
+
+'''
+
+velocidade = 61
+
+km = 90
+
+RADAR_1 = 60  #  Velocidade máxima do redar 1
+LOCAL_1 = 100  #  Local onde o radar 1 está
+RADAR_RANGE = 1  #  A distância onde radar pega
 
 
+# if velocidade > RADAR_1:
+#     print('Velocidade carro passo do radar 1')
 
 
+# if  km >= (LOCAL_1 - RADAR_RANGE) and km <= (LOCAL_1 + RADAR_RANGE) and velocidade > RADAR_1:
+#     print('Carro multado no radar 1')
+
+
+#  OUTRA MANEIRA DE FAZER 
+
+val_carro_pass_radar1 = velocidade > RADAR_1
+
+carro_multado_radar1 = km >= (LOCAL_1 - RADAR_RANGE) and km <= (LOCAL_1 + RADAR_RANGE)
+
+if val_carro_pass_radar1:
+    print('Velocidade carro passo do radar 1')
+
+if carro_multado_radar1:
+    print('Carro multado no radar 1')
 
 
 
