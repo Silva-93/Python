15cfe0ec12a89fa386a90a23df1df8a2d3c83caa
Silva-93 <117053314+Silva-93@users.noreply.github.com>
2022-12-08 20:18:00 -0300

    Create repeticao_while.py

---
+++ PYTHON/repeticao_while.py
@@ -0,0 +1,75 @@
+'''
+    While (enquanto)
+
+    Executa uma ação enquanto ela for verdadeira.
+'''
+
+
+# contador = 0
+
+# while contador < 100:
+#     contador += 1
+#     print(contador)
+
+#     if contador == 10:
+#         print('Chegou no 10!')
+#         continue
+
+#     if contador == 20:
+#         print('Chegou no 20!')
+#         break
+
+# print('Acabou!')
+
+
+qtd_linha = 5
+
+qtd_coluna = 5
+
+linha = 1
+
+while linha <= qtd_linha:
+    coluna = 1
+    
+    while coluna <= qtd_coluna:
+
+        print(linha)
+
+        coluna += 1
+    
+    linha +=1
+
+
+print('Acabou!')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
