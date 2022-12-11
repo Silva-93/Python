8dc94c1477c2782ad436d8ea60594927dde4327e
Silva-93 <117053314+Silva-93@users.noreply.github.com>
2022-12-11 13:11:34 -0300

    Update repeticao_while.py

--- PYTHON/repeticao_while.py
+++ PYTHON/repeticao_while.py
@@ -65,8 +65,64 @@
 
 
 
+while True:
+    n1 = input('Digite um número.\nNúmero: ')
+    n2 = input('Digite outro número.\nNúmero: ')
+    operador = input('Digite o operador para realizar a conta. [+ - / *].\nOperador: ')
+
+    numerosValidos = None
+
+    numero1 = 0
+    numero2 = 0
+    
+    try:
+        #  Convertendo para números float
+        numero1 = float(n1)
+        numero2 = float(n2)
+
+        #  Validando se são números válidos
+        numerosValidos = True    
+    except:
+        numerosValidos = None
+
+    if numerosValidos is None:
+        print('Há um ou mais números inválidos digitados.')
+        continue
+
+    operadoresPermitidos = '+-/*'
 
 
+    if operador not in operadoresPermitidos:
+        print('Operador inválido.')
+        continue
+
+    if len(operador) > 1:
+        print('Digite apenas um operador.')
+        continue
+    
+    print('Realizando a sua conta. Confira o resultado abaixo.')
+    if operador == '+':
+        print(f'O resultado de {numero1} + {numero2} é {numero1 + numero2}')
+
+    elif operador == '-':
+        print(f'O resultado de {numero1} - {numero2} é {numero1 - numero2}')
+
+    elif operador == '/':
+        print(f'O resultado de {numero1} / {numero2} é {numero1 / numero2}')
+
+    elif operador == '*':
+        print(f'O resultado de {numero1} * {numero2} é {numero1 * numero2}')
+    
+    else:
+        print('Tem algo de errado. Tente novamente.')
+
+    
+    sair = input('Quer sair? [s]air: ')
+    #  deixar as letras em minúsculo e verifica se começa com 's'
+    sair = sair.lower().startswith('s')
+
+    if sair is True:
+        break
 
 
 
