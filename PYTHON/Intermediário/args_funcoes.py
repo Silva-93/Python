b24ce30ec9f29ce584c1b85d30c1f0a5cd0cbb1e
Silva-93 <117053314+Silva-93@users.noreply.github.com>
2022-12-26 21:09:26 -0300

    Update args_funcoes.py

---
+++ PYTHON/Intermediário/args_funcoes.py
@@ -0,0 +1,73 @@
+'''
+    *args - Argumentos não nomeados
+    * - *args (empacotamento e despacotamento)
+
+    O *args é muito usado quando você ainda não sabe a quantidade de parâmetros que serão passados na função. 
+'''
+
+
+
+def soma(*args):
+    # é um acumulador, a cada iterção ele será somado com seu próprio valor mais o valor da iteração
+    total = 0  
+
+    for num in args:
+        total += num
+    return total
+
+soma_1_2_3 = soma(1,2,3)  # todos esses valores serão atribuídos ao args em forma de uma tupla
+print(soma_1_2_3)
+
+somaValores = soma(1,2,3,4,5,6,7,8,9)
+print(somaValores)
+
+# Crie uma função que multiplica todos os argumentos
+# não nomeados recebidos
+# Retorne o total para uma variável e mostre o valor
+# da variável.
+
+def multiplicar(*args):
+    totalMult = 1
+    for num in args:
+        totalMult *= num
+    return totalMult
+
+multi = multiplicar(10,10)
+print(multi)
+
+multi2 = multiplicar(2, 2, 2, 2, 2)
+print(multi2)
+
+# Crie uma função fala se um número é par ou ímpar.
+# Retorne se o número é par ou ímpar.
+
+
+def par_impar(n):
+    numero = n % 2 == 0
+
+    if numero:
+        return f'O N° "{n}" é um número PAR'
+    else:
+        return f'O N° "{n}" é um número IMPAR'
+
+
+par = par_impar(2)
+print(par)
+
+impar = par_impar(5)
+print(impar)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
