3b58a94d9e2b94f236546917b28481d52115e13e
Silva-93 <117053314+Silva-93@users.noreply.github.com>
2023-03-04 20:55:13 -0300

    .

--- PYTHON/POO/method_vs_@classmethod_vs_@staticmethod.py
+++ PYTHON/POO/method_vs_@classmethod_vs_@staticmethod.py
@@ -2,8 +2,42 @@
 
 # method - self, método de instância
 
-# @classmethod - cls, método de classe
+# @classmethod - cls, método de classe | Recebe a classe
 
 # @staticmethod - método estático (❌self, ❌cls)
 
 
+class Connection:
+    def __ini__(self, host='local@host'):
+        # ↓ Método de instãncia
+        self.host = host
+        self.user = None
+        self.password = None
+
+    # setter
+    def set_user(self, user):
+        self.user = user
+
+    def set_password(self, password):
+        self.password =password
+
+
+
+    @classmethod  # Método de classe
+    def create_with_auth(cls, user, password):
+        connection = cls()
+        connection.user = user
+        connection.password = password
+        return connection
+
+
+
+    @staticmethod
+    def soma(a, b):
+        return a + b
+
+
+c1 = Connection()
+""" c1.set_user('Luiz')
+c1.set_password('123') """
+print(c1.user)
---
+++ PYTHON/POO/property.py
@@ -0,0 +1,38 @@
+# @property - um getter no modo Pythônico
+
+# getter - um método para obter um valor de determinado atributo
+# cor -> get_cor()
+# modo pythônico - modo do Python de fazer coisas
+
+# @property é uma propriedade do objeto, ela é um método que se comporta como um atributo
+# Geralmente é usada nas seguintes situações:
+# - como getter
+# - p/ evitar quebrar código cliente
+# - p/ habilitar setter
+# - p/ executar ações ao obter um atributo
+
+# Código cliente - é o código que usa seu código
+
+
+######## Seu código ########
+class Caneta:
+    def __init__(self, cor):
+        self.cor_tinta = cor
+
+
+    @property  # getter -> Faz um método se comportar como um atributo, sem a necessidade dos "()"
+    def cor(self):
+        print('Property')
+        return self.cor_tinta
+    
+
+    @property
+    def cor_tampa(self):
+        return 'Tampa'
+
+###### Código cliente ######
+
+caneta = Caneta('Azul')
+
+print(caneta.cor)
+print(caneta.cor_tampa)
