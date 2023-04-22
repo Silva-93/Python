29d5874126ee42504e6c189b8aa1fb997d64ade2
Jouber <117053314+Silva-93@users.noreply.github.com>
2023-04-22 11:45:35 -0300

    .

--- PYTHON/Modulos_comuns_no_python/interacao_com_OS.py
+++ PYTHON/Modulos_comuns_no_python/interacao_com_OS.py
@@ -5,9 +5,29 @@ import os
 
 os.system('cls')
 
+""" 
 print('a' * 80)
 print('a' * 80)
 print('a' * 80)
 print('a' * 80)
 print('a' * 80)
-print('a' * 80)
+print('a' * 80) 
+"""
+
+"""
+    os.path trabalha com caminhos em windows, linux e mac, é um módulo que fornece funções para trabalhar com caminhos de arquivos nos SOs sem precisar se preocupar com as diferenças entre os sistemas.
+    
+    Exemplo do os.path
+    os.path.join('pasta1', 'pasta2', 'arquivo.txt')  -> Junta strings em um único caminho.
+        saída -> 'pasta1\pasta2\arquivo.txt'
+
+    os.path.split  -> Divide um caminho em uma tupla (diretório, arquivo).
+        os.path.split('/home/user/arquivo.txt)
+            saída -> ('/home/user', 'arquivo.txt')
+    
+    os.path.exists  -> Verifica se um caminho especificado existe
+
+    O os.path só trabalha com caminhos de arquivos e não faz nenhuma operação de netrada/saída com arquivos em si.
+"""
+
+caminho = os.path.join('C:\Users\Jouber')
