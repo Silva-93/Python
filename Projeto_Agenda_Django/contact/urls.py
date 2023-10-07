f52f114668a4971be98f31cbce1a86b04f0f23ff
Silva-93 <117053314+Silva-93@users.noreply.github.com>
2023-10-07 19:00:25 -0300

    teste

--- Projeto_Agenda_Django/contact/urls.py
+++ Projeto_Agenda_Django/contact/urls.py
@@ -1,10 +1,14 @@
-from django.urls import path  # type: ignore
+# type: ignore
+
+from django.urls import path  
 from contact import views
 
 app_name= 'contact'
 
 urlpatterns = [
-    path('<int:contact_id>/', views.contact, name='contact'),
-    path('search/', views.search, name='search'),
     path('', views.index, name='index'),
+    path('search/', views.search, name='search'),
+    
+    # contact (CRUD)
+    path('<int:contact_id>/', views.contact, name='contact'),
 ]
--- Projeto_Agenda_Django/contact/views/contact_views.py
+++ Projeto_Agenda_Django/contact/views/contact_views.py
@@ -1,16 +1,21 @@
 from django.shortcuts import get_object_or_404, render, redirect  # type: ignore
 from django.db.models import Q  # type: ignore
 from contact.models import Contact
+from django.core.paginator import Paginator  # type: ignore
 
 # Create your views here.
 def index(request):
     #  Selecionando todos os contatos onde o show for igual a "True"
     contacts = Contact.objects\
         .filter(show=True)\
-        .order_by('-id')[:10]
+        .order_by('-id')
+    
+    paginator = Paginator(contacts, 10)  # mostra 10 contatos por página
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
 
     context = {
-        'contacts': contacts,
+        'page_obj': page_obj,
         'site_title': 'Contatos - '
     }
 
@@ -39,8 +44,12 @@ def search(request):
         )\
         .order_by('-id')
 
+    paginator = Paginator(contacts, 10)  # mostra 10 contatos por página
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
     context = {
-        'contacts': contacts,
+        'page_obj': page_obj,
         'site_title': 'Search - ',
         'search_value': search_value,
     }
