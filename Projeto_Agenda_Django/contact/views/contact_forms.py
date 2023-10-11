ed0794c5329fa2a60f78f024d9a396c585697461
Silva-93 <117053314+Silva-93@users.noreply.github.com>
2023-10-11 19:21:45 -0300

    .

--- Projeto_Agenda_Django/contact/views/contact_forms.py
+++ Projeto_Agenda_Django/contact/views/contact_forms.py
@@ -3,13 +3,23 @@ from django.shortcuts import get_object_or_404, render, redirect
 from django.db.models import Q 
 from contact.models import Contact
 from django.core.paginator import Paginator 
+from django import forms
+
+
+# Criando um formulário com Django
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model: Contact  # Indicando o formulário
+
+        # indicando os campos que serão utilizados
+        fields = ('first_name', 'last_name', 'phone',)
 
 # Create your views here.
 def create(request):
 
 
     context = {
-
+        'form': ContactForm()
     }
 
     return render(
