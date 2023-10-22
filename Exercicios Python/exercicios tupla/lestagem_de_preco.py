4cd2387f2421d2f0fc9ca185c07bac1155ea9c46
Silva-93 <117053314+Silva-93@users.noreply.github.com>
2023-10-22 20:31:02 -0300

    listagem de preço

---
+++ Exercicios Python/exercicios tupla/lestagem_de_preco.py
@@ -0,0 +1,18 @@
+listagem = (
+    'Lápis', 1.75,
+    'Borracha', 2,
+    'Caderno', 15.90,
+    'Estojo', 25
+)
+
+print('-' * 40)
+print(f'{"LISTAGEM DE PREÇOS":^40}')
+print('-' * 40)
+
+for pos in range(0, len(listagem)):
+    if pos % 2 == 0:
+        print(f'{listagem[pos]:.<30}', end='')
+    else:
+        print(f'{listagem[pos]:.>7.2f}')
+
+print('-' * 40)
