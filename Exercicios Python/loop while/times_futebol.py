e25fe98c12f0cb8ab0925a66723acbc4c205a888
Silva-93 <117053314+Silva-93@users.noreply.github.com>
2023-10-21 22:39:15 -0300

    times_incompleto

---
+++ Exercicios Python/loop while/times_futebol.py
@@ -0,0 +1,4 @@
+
+
+times = ('Botafogo', 'Bragantino', 'Flamengo', 'Athletico-PR', 'Grêmio', 'Palmeiras', 'Atlético-MG', 'Fortaleza', 'Fluminense', 'São-Paulo', 'Cuiabá', 'Bahia', 'Internacional', 'Corinthians', 'Cruzeiro', 'Goiás', 'Vasco', 'Santos', 'Coritiba', 'América-MG')
+
