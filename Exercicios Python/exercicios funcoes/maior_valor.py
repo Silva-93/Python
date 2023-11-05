972a4661609f05694f977f3ae5f154c13d93abdd
Silva-93 <117053314+Silva-93@users.noreply.github.com>
2023-11-05 09:45:36 -0300

    Update maior_valor.py

--- Exercicios Python/exercicios funcoes/maior_valor.py
+++ Exercicios Python/exercicios funcoes/maior_valor.py
@@ -2,15 +2,15 @@
     Faça um programa que tenha uma função que tenha o nome maiorValor, que receba vários parâmetros com valores inteiros. O programa deve analizar todos os valores e dizer qual é o maior deles. 
 """
 def maiorValor(*num):
-    maior = max(num)
-    total = len(num)
+    maior_valor = max(num)
+    total_digitos = len(num)
     print('-=' * 30)
     print('Analizando os valores')
     for valores in num:
         print(f'{valores}', end=' ')
     
-    print(f'\nAo todo foram digitados {total} valores.')
-    print(f'O maior valor foi {maior}')
+    print(f'\nAo todo foram digitados {total_digitos} valores.')
+    print(f'O maior valor foi {maior_valor}')
 
 
 maiorValor(1,4,2,6,3,4,8,4,7)
