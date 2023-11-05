ea320c4bdacaaa23a4660d498642a38c217b14a8
Silva-93 <117053314+Silva-93@users.noreply.github.com>
2023-11-05 13:12:13 -0300

    idade para votar

---
+++ Exercicios Python/exercicios funcoes/voto.py
@@ -0,0 +1,22 @@
+""" 
+    Crie um programa que tenha uma função chamada voto() que vai recever como parâmetro o ano de nascimento de uma pessoa, retornando um valor literal indicando se a pessoa tem voto NEGADO, APCIONAL e OBRIGATÓRIO nas eleições.
+"""
+
+
+def voto(nascimento):
+    from datetime import date
+    ano_atual = date.today().year
+    idade = ano_atual - nascimento
+    print('--' * 20)
+    if 16 == idade < 18 or idade >= 65:
+        return f'Com a idade de {idade} o voto é OPCIONAL.'
+    
+    elif idade < 16:
+        return f'Com a idade de {idade} o voto é NEGADO.'
+    
+    else:
+        return f'Com a idade de {idade} o voto é OBRIGATÓRIO.'
+
+nascimento = int(input('Ano de nascimento: '))
+
+print(voto(nascimento))
