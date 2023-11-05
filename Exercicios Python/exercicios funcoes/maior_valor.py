1b7b16e0bc106860b89a8b18ea27a3f91581105d
Silva-93 <117053314+Silva-93@users.noreply.github.com>
2023-11-04 21:05:42 -0300

    Maior valor

---
+++ Exercicios Python/exercicios funcoes/maior_valor.py
@@ -0,0 +1,27 @@
+""" 
+    Faça um programa que tenha uma função que tenha o nome maiorValor, que receba vários parâmetros com valores inteiros. O programa deve analizar todos os valores e dizer qual é o maior deles. 
+"""
+def maiorValor(*num):
+    maior = max(num)
+    total = len(num)
+    print('-=' * 30)
+    print('Analizando os valores')
+    for valores in num:
+        print(f'{valores}', end=' ')
+    
+    print(f'\nAo todo foram digitados {total} valores.')
+    print(f'O maior valor foi {maior}')
+
+
+maiorValor(1,4,2,6,3,4,8,4,7)
+maiorValor(1,4,7)
+maiorValor(0)
+
+
+
+
+
+
+
+
+
