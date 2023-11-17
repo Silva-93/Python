22175149dd88f300d2648b84516a031bd1d2cfda
Silva-93 <117053314+Silva-93@users.noreply.github.com>
2023-11-16 22:42:17 -0300

    criando models (base de dados)

--- Django/contact/models.py
+++ Django/contact/models.py
@@ -1,3 +1,12 @@
-from django.db import models
+from django.db import models  # type: ignore
+from django.utils import timezone  # type: ignore
 
 # Create your models here.
+class Contact(models.Model):
+    # Criando campos da tabela
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254, blank=True)
+    created_date = models.DateTimeField(default=timezone.now)
+    description = models.TextField(blank=True)
