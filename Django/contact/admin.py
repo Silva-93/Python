7f82dbf6e0eed38636b8e5439290a4289dc77dfc
Silva-93 <117053314+Silva-93@users.noreply.github.com>
2023-11-19 15:24:04 -0300

    configurando a página dos contatos do model

--- Django/contact/admin.py
+++ Django/contact/admin.py
@@ -5,4 +5,11 @@ from contact import models
 
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin): 
-    ...
+    list_display = 'id', 'first_name', 'last_name', 'phone',
+    ordering = 'id',  # ordena pelo id, caso coloque o sinal de “-“(‘-id’) vai ordena de forma decrescente.
+    # list_filter = 'created_date',  # cria um filtro de data
+    search_fields = 'id', 'first_name', 'last_name',  # cria um campo de busca
+    list_per_page = 10
+    list_max_show_all = 200
+    list_editable = 'first_name', 'last_name', 
+    list_display_links = 'id',
