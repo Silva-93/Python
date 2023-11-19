5eacd157acf8d7e3e33ea410793fd9f1ef40b60f
Silva-93 <117053314+Silva-93@users.noreply.github.com>
2023-11-19 14:23:40 -0300

    registrando um model no admin do django

--- Django/contact/admin.py
+++ Django/contact/admin.py
@@ -1,3 +1,8 @@
-from django.contrib import admin
+from django.contrib import admin  # type: ignore
+from contact import models
 
 # Register your models here.
+
+@admin.register(models.Contact)
+class ContactAdmin(admin.ModelAdmin): 
+    ...
