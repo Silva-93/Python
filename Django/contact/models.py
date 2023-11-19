c1872fadc1a20716a34a460d570b913baf57ba47
Silva-93 <117053314+Silva-93@users.noreply.github.com>
2023-11-19 19:23:48 -0300

    Configurando a categoria (FK)

--- Django/contact/models.py
+++ Django/contact/models.py
@@ -2,6 +2,14 @@ from django.db import models  # type: ignore
 from django.utils import timezone  # type: ignore
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    
+    def __str__(self) -> str:
+        return self.name
+
+
 class Contact(models.Model):
     # Criando campos da tabela
     first_name = models.CharField(max_length=50)
@@ -12,6 +20,7 @@ class Contact(models.Model):
     description = models.TextField(blank=True)
     show = models.BooleanField(default=True)
     picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m')
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True)
 
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
