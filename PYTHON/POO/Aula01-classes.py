5f2248cad68ab7416c472258c6746c05a52b6c7e
Silva-93 <117053314+Silva-93@users.noreply.github.com>
2023-12-06 19:17:00 -0300

    Aula-1 classes

--- PYTHON/POO/Aula01-classes.py
+++ PYTHON/POO/Aula01-classes.py
@@ -6,34 +6,22 @@
 
 # string = 'Luiz'  # str
 
-# print(string.upper())
+# print(string.upper())|
 
 # print(isinstance(string, str))
 
 
 # Criando uma classe com dois atributos
 class Pessoa:
+    # atributos da classe ↓
     def __init__(self, nome, sobrenome):
         self.nome = nome
         self.sobrenome = sobrenome
 
 p1 = Pessoa('Jouber', 'Vicente')
 
-""" 
+
 print(p1.nome)
 print(p1.sobrenome)
-"""
-
-# Métodos em instâcias de classes Python
-
-class Carro:
-    def __init__(self, nome):
-        self.nome = nome
-
-    # Métodos da classe
-    def acelerar(self):
-        print(f'{self.nome} está acelerando')
 
 
-celta = Carro('Celta')
-celta.acelerar()  # chamando a classe com o método.
