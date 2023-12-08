8596f24efd4e34e92126f45f9c8c5db18b70d081
Silva-93 <117053314+Silva-93@users.noreply.github.com>
2023-12-07 21:33:52 -0300

    Aula10_metodos_de_classe_metodos_de_fabrica

--- PYTHON/POO/Aula10_metodos_de_classe_metodos_de_fabrica.py
+++ PYTHON/POO/Aula10_metodos_de_classe_metodos_de_fabrica.py
@@ -1,6 +1,8 @@
 """ 
     Métodos de classe e factories (fábricas)
     Métodos de classe -> São métodos onde "self" será "cls", ou seja, ao invés de receber a instâcia no primeiro parâmetro, receberá a prória classe. 
+
+    factories -> Cria um novo objeto da classe
 """
 
 
@@ -13,5 +15,29 @@ class Pessoa:
 
 
     # Método de classe
-    @classmethod
-    def metodo_de_clase
+    @classmethod  
+    def metodo_de_classe(cls):
+        print('Hey')
+
+
+    # método que cria um novo objeto
+    @classmethod  
+    def pessoa_mais_de_50_anos(cls, nome):
+        return cls(nome, 50)
+
+
+    @classmethod  
+    def pessoa_sem_nome(cls, idade):
+        return cls('Anônima', idade)
+
+
+
+p1 = Pessoa('José', 32)
+p2 = Pessoa.pessoa_mais_de_50_anos('Helena')
+p3 = Pessoa.pessoa_sem_nome(20)
+
+print(Pessoa.ano)
+Pessoa.metodo_de_classe()
+
+print(p2.nome, p2.idade)
+print(p3.nome, p3.idade)
