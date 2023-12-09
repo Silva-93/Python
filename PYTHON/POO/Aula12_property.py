28035174db94d8dedd11393a73fd438fa9c357fc
Silva-93 <117053314+Silva-93@users.noreply.github.com>
2023-12-09 14:25:04 -0300

    Aula12_property (completo)

--- PYTHON/POO/Aula12_property.py
+++ PYTHON/POO/Aula12_property.py
@@ -2,7 +2,7 @@
     @property -> Um getter no modo pythônico. 
     getter - é um método para obter um valor de um determinado atributo
 
-    @property -> É uma propriedade do objeto, ela é um método que se comporta como um atributo. Geralmente é usada nas seguintes situações:
+    @property -> É uma propriedade do objeto, ele faz um método se comportar como um atributo, com isso não é necessário usar os "()" para chamar o método. Geralmente é usada nas seguintes situações:
         como getter
         p/ evitar quebrar código cliente
         p/ habilitar o setter
@@ -11,10 +11,29 @@
 
 class Caneta:
     def __init__(self, cor):
-        self.cor = cor
+        self.cor_tinta = cor
 
+
+
+    # getter normalmente usado em outras linguagens de programação
+    # def get_cor(self):
+    #     return self.cor
+
+
+    # getter usado em python
+    @property
+    def cor(self):
+        print('Usando a PROPERTY.')
+        return self.cor_tinta
+
+
+    @property
+    def cor_tampa(self):
+        return 'Tampa azul'
 
 
 
 caneta = Caneta('Azul')
+# print(caneta.get_cor())
 print(caneta.cor)
+print(caneta.cor_tampa)
