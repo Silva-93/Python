3dd13f1d214152f154b45dbfdf2800ec83a8dfc9
Silva-93 <117053314+Silva-93@users.noreply.github.com>
2023-12-09 17:09:21 -0300

    Aula15_relacao_entre_classes_(associacao)

---
+++ PYTHON/POO/Aula15_relacao_entre_classes_(associacao).py
@@ -0,0 +1,39 @@
+""" 
+    Relações entre classes: associação, agregação e composição.
+
+    Associação é um tipo de relação onde os objetos estão ligados dentro do sistema. Essa é relação mais comum entre objetos e tem subconjuntos como agregação e composição. Geralmente, temos um associação quando um objeto tem um atributo que referencia outro objeto. A associação não especifica como um objeto controla o ciclo de vida de outro objeto. 
+"""
+
+class Escritor:
+    def __init__(self, nome):
+        self.nome = nome
+        self.maquina = None
+
+
+    @property
+    def maquina(self):
+        return self._maquina
+    
+
+    @maquina.setter
+    def maquina(self, maquina):
+        self._maquina = maquina
+
+
+
+class MaquinaDeEscrever:
+    def __init__(self, nome):
+        self.nome = nome
+
+
+    def escrever(self):
+        return f'{self.nome} está escrevendo.'
+    
+
+
+escritor = Escritor('Luiz')
+datilografia = MaquinaDeEscrever('Maquina datilografia')
+escritor.maquina = datilografia  # associando a classe "Escritor" com a classe "MaquinaDeEscrever"
+
+print(datilografia.escrever())  # saida -> Maquina datilografia está escrevendo.
+print(escritor.maquina.escrever())  # saida -> Maquina datilografia está escrevendo.
