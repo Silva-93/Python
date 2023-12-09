61e3777e62300f49d3082518a00904712278ef0f
Silva-93 <117053314+Silva-93@users.noreply.github.com>
2023-12-09 19:03:49 -0300

    del

--- Python_Test/calculadora.py
+++
@@ -1,7 +0,0 @@
-def soma(x, y):
-
-    #verifica se "x" é um número inteiro ou real
-    assert isinstance(x, (int, float)), 'x deve ser um número inteiro ou real'  
-    assert isinstance(y, (int, float)), 'y deve ser um número inteiro ou real'  
-    return x + y
-
--- Python_Test/test_calculadora.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from calculadora import soma
-
-class TestCalculadora(unittest.TestCase):
-    def test_soma_5_com_5_resultado_10(self):
-        self.assertEqual(soma(5, 5), 10)
-
-
-unittest.main(verbosity=2)
