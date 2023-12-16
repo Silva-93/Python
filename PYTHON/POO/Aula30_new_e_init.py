221444686fd464c7f12d5dcf8ada8e9ef4bb8343
Silva-93 <117053314+Silva-93@users.noreply.github.com>
2023-12-15 21:25:47 -0300

    Aula30_new_e_init (incompleto)

---
+++ PYTHON/POO/Aula30_new_e_init.py
@@ -0,0 +1,41 @@
+""" 
+    __new__ e __init__ em classes python
+
+    __new__ é o método responsável por criar e retornar o novo objeto. Por isso, new recebe "cls".
+    __new__ deve retornar o novo objeto.
+
+    __init__ é o método responsável por inicializar a instâcia. Por isso, init recebe self.
+    __init__ deve retornar nada (None)
+
+    object é a super classe de uma classe
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
