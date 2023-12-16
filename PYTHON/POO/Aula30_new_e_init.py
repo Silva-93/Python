7520d666c233fcf1ee22d4b4bb5ab4d88e5c6f12
Silva-93 <117053314+Silva-93@users.noreply.github.com>
2023-12-16 18:58:13 -0300

    Aula30_new_e_init

--- PYTHON/POO/Aula30_new_e_init.py
+++ PYTHON/POO/Aula30_new_e_init.py
@@ -11,18 +11,22 @@
 """
 
 
+class A:
+    def __new__(cls, *args, **kwargs):
+        instancia = super().__new__(cls)
+        return instancia
 
 
+    def __init__(self):
+        print('Sou o init')
 
 
+    def __repr__(self):
+        return 'A()'
 
 
-
-
-
-
-
-
+a = A()
+# print(a)
 
 
 
