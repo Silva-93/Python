590bf2986b956234bce8a0f0d0672cfb3c0af297
Silva-93 <117053314+Silva-93@users.noreply.github.com>
2023-12-16 20:37:15 -0300

    Aula34_funcoes_decoradores_e_decoradores_com_classe

--- PYTHON/POO/Aula34_funcoes_decoradores_e_decoradores_com_classe.py
+++ PYTHON/POO/Aula34_funcoes_decoradores_e_decoradores_com_classe.py
@@ -1,13 +1,18 @@
 """ 
     Funções decoradoras e decoradores com classes
 """
+
+# criando uma função quer vai servir como um __repr__
+def my_repr(self) -> str:
+    class_name = self.__class__.__name__
+    class_dict = self.__dict__
+    class_repr = f'{class_name} ({class_dict})'
+    return class_repr
+
+
+
 # função decoradora
 def add_repr(cls):
-    def my_repr(self) -> str:
-        class_name = self.__class__.__name__
-        class_dict = self.__dict__
-        class_repr = f'{class_name} ({class_dict})'
-        return class_repr
     cls.__repr__ = my_repr
     return cls
 
