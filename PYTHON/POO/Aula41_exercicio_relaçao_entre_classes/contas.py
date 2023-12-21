49d14ca2954a9ed8bec51370cbc9c8efb4b87b7c
Silva-93 <117053314+Silva-93@users.noreply.github.com>
2023-12-21 15:31:04 -0300

    criação da classe conta poupança

--- PYTHON/POO/Aula41_exercicio_relaçao_entre_classes/contas.py
+++ PYTHON/POO/Aula41_exercicio_relaçao_entre_classes/contas.py
@@ -17,17 +17,31 @@ class Conta(abc.ABC):
 
 
     def detalhes(self, msg=''):
-        print(f'O seu saldo é {self.saldo:.2f} {msg}')
+        print(f'O seu saldo é [{self.saldo:.2f}] {msg}')
+        print('--' * 20)
 
 
 
+# ContaPoupanca herda de Conta
+class ContaPoupanca(Conta):
+    def sacar(self, valor):
+        valor_pos_saque = self.saldo - valor
+
+        if valor_pos_saque >= 0:
+            self.saldo -= valor
+            self.detalhes(f'(SAQUE: {valor})')
+            return self.saldo
+        
+        print('Não foi possível savar o valor desejado.')
+        self.detalhes(f'(SAQUE NEGADO! Valor "{valor}" é insuficiente.)')
 
 
 
+if __name__ == '__main__':
+    conta_poupanca_1 = ContaPoupanca(111, 222, 10)
+    conta_poupanca_1.sacar(1)
 
-
-
-
+    conta_poupanca_1.depositar(2)
 
 
 
