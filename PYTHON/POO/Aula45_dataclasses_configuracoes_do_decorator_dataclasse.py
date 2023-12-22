eebd922c723511cf4d434d746c2b2ede54a57527
Silva-93 <117053314+Silva-93@users.noreply.github.com>
2023-12-22 20:01:55 -0300

    Aula45_dataclasses_configuracoes_do_decorator_dataclasse

---
+++ PYTHON/POO/Aula45_dataclasses_configuracoes_do_decorator_dataclasse.py
@@ -0,0 +1,32 @@
+""" 
+    dataclasses
+
+    O módulo dataclasses fornece um decorador e funções para criar métodos como __init__(), __repr__(), __eq__(), entre outros, em classes definidas pelo usuário.
+    Em resumo, dataclasses são syntax sugar para criar classes normais.
+
+    __post_init__() -> é executado logo após a dataclasse
+
+    @dataclass(init=False)  -> desativa o __init__()
+    @dataclass(frozen=False)  -> congela a classe, assim não é possível "setar" atributos
+    @dataclass(order=True)  -> Torna possível criar uma ordenação
+"""
+
+
+from dataclasses import dataclass
+
+
+@dataclass
+class Pessoa:
+    nome: str
+    sobrenome: str
+
+    def __post_init__(self):
+        self.nome_completo = f'{self.nome} {self.sobrenome}'
+
+
+
+if __name__ == '__main__':
+    p1 = Pessoa('jouber', 'vicente')
+
+    print(p1)
+    print(p1.nome_completo)
