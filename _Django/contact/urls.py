eeb7715f1c6b4c2113518b7ca388c416c062cc47
Silva-93 <117053314+Silva-93@users.noreply.github.com>
2023-12-23 21:34:02 -0300

    Criando a view, url e template para "delete" (apagar contatos)

--- _Django/contact/urls.py
+++ _Django/contact/urls.py
@@ -12,4 +12,5 @@ urlpatterns = [
     path('contact/create/', views.create, name='create'),  # type: ignore
     path('contact/<int:contact_id>/detail/', views.contact, name='contact'),  # type: ignore
     path('contact/<int:contact_id>/update/', views.update, name='update'),  # type: ignore
+    path('contact/<int:contact_id>/delete/', views.delete, name='delete'),  # type: ignore
 ] 
