f930e1e63b0aef4bda7bd4f954a8b4c643a7c84f
Silva-93 <117053314+Silva-93@users.noreply.github.com>
2024-01-23 19:52:39 -0300

    aula02_metacaracteres

---
+++ PYTHON/expressões regulares/aula02_metacaracteres.py
@@ -0,0 +1,40 @@
+import re
+
+# . -> significa "qualquer caracter" por padrão ele não seleciona uma quebra de linha
+# ^ -> 
+# $ -> 
+# * -> 
+# + -> 
+# ? -> 
+# {} -> 
+# [] -> significa um "conjunto" de caracteres
+# \ -> 
+# | -> significa "ou"
+# () -> 
+
+
+texto = '''
+João trouxe       flores para sua amada namorada em 10 de janeiro de 1970, Maria era o nome dela.
+
+
+
+Foi um ano excelente na vida de joão. Teve 5 filhos, todos adultos atualmente. maria, hoje sua esposa, ainda faz aquile café com pão de queijo nas tardes de domingo. Também né! Sendo a bua mineira que é, nunca esquece seu famoso pão de queijo.
+Não canso de ouvir a Maria: 
+"Jooooooooooãoooooooo, o café té prontinho aqui. Veeeemm!"
+'''
+
+print(re.findall(r'João|Maria', texto) )  # retorna uma lista com João e Maria
+
+print(re.findall(r'ad....', texto))  # a ocorrência começa com "ad" o resto pode ser qualquer caracter. A quantidade de pontos indica aquandidade de caracteres após o início
+
+print(re.findall(r'[Jj]oão|[Mm]aria', texto))
+
+print(re.findall(r'[a-zA-Z]aria', texto))  # [a-z] -> qualquer letra de a até z
+
+print(re.findall(r'jOãO|MaRia', texto, flags=re.I))  # "I" (IGNORECASE) não diferencia letas maiúscula de menúscula 
+
+
+
+
+
+
