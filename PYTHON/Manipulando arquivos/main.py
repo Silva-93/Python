f74d5879ce071d6cb3ef388b120fe70d42025f08
Silva-93 <117053314+Silva-93@users.noreply.github.com>
2024-03-17 18:09:18 -0300

    caminhos (incompleto)

---
+++ PYTHON/Manipulando arquivos/main.py
@@ -0,0 +1,51 @@
+from pathlib import Path
+# File System -> É responsável por das nomes, armazenar e recuperar os aruqivos no seu SO
+
+current_path = Path()
+absolute_path = Path().absolute()
+
+# criando um arquivo
+create_file = Path('teste.txt')
+# create_file.touch()
+# create_file.unlink()  # excluindo o arquivo
+
+
+print(current_path)
+print(absolute_path)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
